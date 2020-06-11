03d831070543bed572474353cf1dc2c9dbf315bc
Anton <novoselov.ab@gmail.com>
2020-06-11 14:35:17 -0700

    first working state

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,5 +1,7 @@
+use rand::rngs::ThreadRng;
+use rand::Rng;
 use std::fs;
-use std::path::{PathBuf};
+use std::path::PathBuf;
 
 #[derive(Default)]
 pub struct Screen {
@@ -9,34 +11,110 @@ pub struct Screen {
 const SCREEN_WIDTH: usize = 64;
 const SCREEN_HEIGHT: usize = 32;
 
+// TODO: try const
+pub static FONT_DATA: [u8; 80] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
+
 impl Screen {
     pub fn new() -> Self {
         Screen {
-            data: vec![0; SCREEN_WIDTH * SCREEN_HEIGHT]
+            data: vec![0; SCREEN_WIDTH * SCREEN_HEIGHT],
         }
     }
 
-    pub fn clear(& mut self) {
+    pub fn clear(&mut self) {
         self.data = vec![0; SCREEN_WIDTH * SCREEN_HEIGHT];
     }
 
-    pub fn set_pixel(& mut self, x: usize, y: usize, v: u8) {
-        let xw = x % SCREEN_WIDTH;
-        let yw = y % SCREEN_HEIGHT;
+    pub fn set_pixel(&mut self, x: usize, y: usize, v: bool) {
+        self.data[x + y * SCREEN_WIDTH] = v as u8;
+    }
 
-        self.data[yw * SCREEN_HEIGHT + xw] = v;
+    pub fn get_pixel(&mut self, x: usize, y: usize) -> bool {
+        self.data[x + y * SCREEN_WIDTH] == 1
+    }
+
+    pub fn draw_sprite(&mut self, x: usize, y: usize, sprite: &[u8]) -> bool {
+        let rows = sprite.len();
+        let mut collision = false;
+        for j in 0..rows {
+            let row = sprite[j];
+            for i in 0..8 {
+                let new_value = row >> (7 - i) & 0x01;
+                if new_value == 1 {
+                    let xi = (x + i) % SCREEN_WIDTH;
+                    let yj = (y + j) % SCREEN_HEIGHT;
+                    let old_value = self.get_pixel(xi, yj);
+                    if old_value {
+                        collision = true;
+                    }
+                    self.set_pixel(xi, yj, (new_value == 1) ^ old_value);
+                }
+            }
+        }
+        return collision;
     }
 }
 
+const KEY_COUNT: usize = 16;
+
+#[derive(Default)]
+pub struct Keypad {
+    keys: [bool; KEY_COUNT],
+}
+
+impl Keypad {
+    pub fn is_pressed(&self, key: u8) -> bool {
+        self.keys[key as usize]
+    }
+
+    pub fn set(&mut self, index: u8, down: bool) {
+        self.keys[index as usize] = down;
+    }
+
+    pub fn reset(&mut self) {
+        self.keys = [false; KEY_COUNT];
+    }
+
+    pub fn get_pressed(&self) -> Option<u8> {
+        for i in 0..self.keys.len() {
+            if self.is_pressed(i as u8) {
+                return Some(i as u8);
+            }
+        }
+        None
+    }
+}
 
 #[derive(Default)]
 pub struct Emulator {
     _halt: bool,
-    screen: Screen,
+    pub screen: Screen,
+    pub keypad: Keypad,
     memory: Vec<u8>,
-    stack: Vec<u16>,
-    data_registers: [u8; 16],
-    pc: u16,
+    pub stack: Vec<u16>,
+    pub rs: [u8; 16], // Data registers
+    pub ri: u16,      // I register
+    pub pc: u16,
+    rng: ThreadRng,
+    pub delay: u8,
+    pub total_dt: f32,
 }
 
 impl Emulator {
@@ -67,6 +145,9 @@ impl Emulator {
         self.memory.resize(65535, 0);
 
         self.screen.clear();
+        self.keypad.reset();
+
+        self.delay = 0;
 
         self._halt = false;
 
@@ -75,14 +156,18 @@ impl Emulator {
             self.memory[i + 0x200] = contents[i];
         }
 
-        // Reset data_registers
-        for i in 0..self.data_registers.len() {
-            self.data_registers[i] = 0;
+        // Copy, use splice?
+        for i in 0..FONT_DATA.len() {
+            self.memory[i] = FONT_DATA[i];
+        }
+
+        // Reset rs
+        for i in 0..self.rs.len() {
+            self.rs[i] = 0;
         }
 
         self.pc = 0x200;
-
-        
+        self.rng = rand::thread_rng();
 
         // println!("Content:");
         // for b in contents {
@@ -97,13 +182,25 @@ impl Emulator {
         // }
     }
 
+    pub fn update_timer(&mut self, dt: f32) {
+        if self.delay > 0 {
+            self.total_dt += dt;
+            const TIMER_PERIOD: f32 = 1.0/60.0;
+            while self.total_dt > TIMER_PERIOD {
+                self.total_dt -= TIMER_PERIOD;
+                self.delay -= 1;
+            }
+        }
+    }
+
     pub fn execute_instruction(&mut self) {
-        let opcode = ((self.memory[self.pc as usize] as u16) << 8) | (self.memory[(self.pc as usize) + 1] as u16);
+        let opcode = ((self.memory[self.pc as usize] as u16) << 8)
+            | (self.memory[(self.pc as usize) + 1] as u16);
         let nibbles = (
             (opcode & 0xF000) >> 12 as u8,
             (opcode & 0x0F00) >> 8 as u8,
-            (opcode & 0x00F0) >> 4  as u8,
-            (opcode & 0x000F) >> 0  as u8,
+            (opcode & 0x00F0) >> 4 as u8,
+            (opcode & 0x000F) >> 0 as u8,
         );
         let nnn = (opcode & 0x0FFF) as u16;
         let nn = (opcode & 0x00FF) as u8;
@@ -117,7 +214,7 @@ impl Emulator {
             (0, 0, 0xE, 0) => {
                 // clear screen
                 self.screen.clear();
-            },
+            }
             (0, 0, 0xE, 0xE) => {
                 // Return from a subroutine
                 if let Some(adr) = self.stack.pop() {
@@ -127,62 +224,176 @@ impl Emulator {
             (0, _, _, _) => {
                 // Ignore 0NNN ?
                 panic!("0NNN");
-            },
+            }
             (1, _, _, _) => {
                 // jump to adress
                 self.pc = nnn;
-            },
+            }
             (2, _, _, _) => {
                 // Execute subroutine starting at address NNN
                 self.stack.push(self.pc);
                 self.pc = nnn;
-            },
+            }
             (3, _, _, _) => {
                 // Skip the following instruction if the value of register VX equals NN
-                if self.data_registers[x] == nn {
+                if self.rs[x] == nn {
                     self.pc += 2;
                 }
-            },
+            }
             (4, _, _, _) => {
                 // Skip the following instruction if the value of register VX is not equal to NN
-                if self.data_registers[x] != nn {
+                if self.rs[x] != nn {
                     self.pc += 2;
                 }
-            },            
+            }
             (5, _, _, 0) => {
                 // Skip the following instruction if the value of register VX is equal to the value of register VY
-                if self.data_registers[x] != self.data_registers[y] {
+                if self.rs[x] == self.rs[y] {
                     self.pc += 2;
                 }
-            },            
+            }
             (6, _, _, _) => {
                 // Store number NN in register VX
-                self.data_registers[x] = nn;
-            },            
+                self.rs[x] = nn;
+            }
             (7, _, _, _) => {
                 // Add the value NN to register VX
-                self.data_registers[x] += nn;
-            },            
+                self.rs[x] =self.rs[x].wrapping_add(nn);
+            }
             (8, _, _, 0) => {
                 // Store the value of register VY in register VX
-                self.data_registers[x] = self.data_registers[y];
-            },                   
-            (8, _, _, 1) => { 
+                self.rs[x] = self.rs[y];
+            }
+            (8, _, _, 1) => {
                 // Set VX to VX OR VY
-                self.data_registers[x] = self.data_registers[x] | self.data_registers[y];
-            },                   
+                self.rs[x] = self.rs[x] | self.rs[y];
+            }
             (8, _, _, 2) => {
                 // Set VX to VX AND VY
-                self.data_registers[x] = self.data_registers[x] & self.data_registers[y];
-            },                   
+                self.rs[x] = self.rs[x] & self.rs[y];
+            }
             (8, _, _, 3) => {
                 // Set VX to VX XOR VY
-                self.data_registers[x] = self.data_registers[x] ^ self.data_registers[y];
-            },                   
-            _ => {
-
+                self.rs[x] = self.rs[x] ^ self.rs[y];
             }
+            (8, _, _, 4) => {
+                // Add the value of register VY to register VX, Set VF to carry (0/1)
+                let (res, overflow) = self.rs[x].overflowing_add(self.rs[y]);
+                self.rs[0xF] = overflow as u8;
+                self.rs[x] = res;
+            }
+            (8, _, _, 5) => {
+                // Subtract the value of register VY from register VX, Set VF to !borrow
+                let (res, overflow) = self.rs[x].overflowing_sub(self.rs[y]);
+                self.rs[0xF] = !overflow as u8;
+                self.rs[x] = res;
+            }
+            (8, _, _, 6) => {
+                // Shifts VX right by one. VF is set to the value of
+                // the least significant bit of VX before the shift.
+                self.rs[0xF] = self.rs[x] & 0x1;
+                self.rs[x] = self.rs[x] >> 1;
+            }
+            (8, _, _, 7) => {
+                // Set register VX to the value of VY minus VX. Set VF to 00 if a borrow occurs. Set VF to 01 if a borrow does not occur
+                let (res, overflow) = self.rs[y].overflowing_sub(self.rs[x]);
+                self.rs[0xF] = !overflow as u8;
+                self.rs[x] = res;
+            }
+            (8, _, _, 0xE) => {
+                // Shifts VX left by one. VF is set to the value of
+                // the most significant bit of VX before the shift.
+                self.rs[0xF] = self.rs[x] >> 7;
+                self.rs[x] = self.rs[x] << 1;
+            }
+            (9, _, _, 0) => {
+                // Skip the following instruction if the value of register VX is not equal to the value of register VY
+                if self.rs[x] != self.rs[y] {
+                    self.pc += 2;
+                }
+            }
+            (0xA, _, _, _) => {
+                // Store memory address NNN in register I
+                self.ri = nnn;
+            }
+            (0xB, _, _, _) => {
+                // Jump to address NNN + V0
+                self.pc = nnn + self.rs[0] as u16;
+            }
+            (0xC, _, _, _) => {
+                // Set VX to a random number with a mask of NN
+                self.rs[x] = self.rng.gen::<u8>() & nn;
+            }
+            (0xD, _, _, _) => {
+                // Draw a sprite at position VX, VY with N bytes of sprite data starting at the address stored in I
+                // Set VF to 01 if any set pixels are changed to unset, and 00 otherwise
+                let c = self.screen.draw_sprite(
+                    self.rs[x] as usize,
+                    self.rs[y] as usize,
+                    &self.memory[self.ri as usize..(self.ri + n as u16) as usize],
+                );
+                self.rs[0xF] = c as u8;
+            }
+            (0xE, _, 0x9, 0xE) => {
+                // Skip the following instruction if the key corresponding to the hex value currently stored in register VX is pressed
+                if self.keypad.is_pressed(self.rs[x]) {
+                    self.pc += 2;
+                }
+            }
+            (0xE, _, 0xA, 0x1) => {
+                // Skip the following instruction if the key corresponding to the hex value currently stored in register VX is not pressed
+                if !self.keypad.is_pressed(self.rs[x]) {
+                    self.pc += 2;
+                }
+            }
+            (0xF, _, 0x0, 0x7) => {
+                // Store the current value of the delay timer in register VX
+                self.rs[x] = self.delay;
+            }
+            (0xF, _, 0x0, 0xA) => {
+                // Wait for a keypress and store the result in register VX
+                if let Some(key) = self.keypad.get_pressed() {
+                    self.rs[x] = key;
+                } else {
+                    self.pc -= 2;
+                }
+            }
+            (0xF, _, 0x1, 0x5) => {
+                // Set the delay timer to the value of register VX
+                self.delay = self.rs[x];
+            }
+            (0xF, _, 0x1, 0x8) => {
+                // Set the sound timer to the value of register VX
+                // no sound?? :(
+            }
+            (0xF, _, 0x1, 0xE) => {
+                // Add the value stored in register VX to register I
+                self.ri += self.rs[x] as u16;
+            }
+            (0xF, _, 0x2, 0x9) => {
+                // Set I to the memory address of the sprite data corresponding to the hexadecimal digit stored in register VX
+                self.ri = self.rs[x] as u16 * 5;
+            }
+            (0xF, _, 0x3, 0x3) => {
+                // Store the binary-coded decimal equivalent of the value stored in register VX at addresses I, I + 1, and I + 2
+                self.memory[self.ri as usize] = self.rs[x] / 100;
+                self.memory[self.ri as usize + 1] = (self.rs[x] / 10) % 10;
+                self.memory[self.ri as usize + 2] = self.rs[x] % 10;
+            }
+            (0xF, _, 0x5, 0x5) => {
+                // Store the values of registers V0 to VX inclusive in memory starting at address I is set to I + X + 1 after operation²
+                self.memory[(self.ri as usize)..(self.ri + x as u16 + 1) as usize]
+                    .copy_from_slice(&self.rs[0..(x as usize + 1)]);
+                self.ri += (x + 1) as u16;
+            }
+            (0xF, _, 0x5, 0x6) => {
+                // Fill registers V0 to VX inclusive with the values stored in memory starting at address I is set to I + X + 1 after operation²
+                self.rs[0..(x as usize + 1)].copy_from_slice(
+                    &self.memory[(self.ri as usize)..(self.ri + x as u16 + 1) as usize],
+                );
+                self.ri += (x + 1) as u16;
+            }
+            _ => {}
         }
     }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use imgui::*;
-use std::rc::Rc;
 use glob::glob;
-use std::path::{PathBuf};
+use imgui::*;
+use std::path::PathBuf;
+use std::rc::Rc;
 
 mod app;
 mod chip8;
 
-const SCREEN_SIZE: (u32, u32) = (64, 64);
+const SCREEN_SIZE: (u32, u32) = (64, 32);
 
 fn find_roms() -> glob::Paths {
     let exe_path = std::env::current_exe();
@@ -15,20 +15,17 @@ fn find_roms() -> glob::Paths {
     glob(rom_path.join("**/*.ch8").to_str().unwrap()).unwrap()
 }
 
-
-struct MyApp
-{
+struct MyApp {
     _roms: Vec<PathBuf>,
-    _emulator: chip8::Emulator
+    _emulator: chip8::Emulator,
 }
 
-impl MyApp
-{
+impl MyApp {
     fn new() -> Self {
         let _roms = find_roms().map(|res| res.unwrap()).collect();
         MyApp {
             _roms,
-            _emulator: chip8::Emulator::new()
+            _emulator: chip8::Emulator::new(),
         }
     }
 
@@ -36,43 +33,57 @@ impl MyApp
         let window = imgui::Window::new(im_str!("ROMs"));
         window
             .size([400.0, 600.0], Condition::FirstUseEver)
-            .build(&ui, move || {
+            .build(&ui, || {
                 for rom in &self._roms {
-
-                    if ui.button(&ImString::new(rom.to_str().unwrap()), [0 as f32, 0 as f32]) {
+                    if ui.button(&ImString::new(rom.file_name().unwrap().to_str().unwrap()), [0 as f32, 0 as f32]) {
                         self._emulator.run(rom);
                     }
                 }
-
-                if !self._emulator.is_halting() {
-                    self._emulator.execute_instruction()
-                }
-
-                let width = SCREEN_SIZE.0 as usize;
-                let height = SCREEN_SIZE.1 as usize;
-                for x in 0..width {
-                    for y in 0..height {
-                        let x0 = x * 4;
-                        let y0 = y * 4;
-                        screen_raw[y0 * width + x0] = rand::random::<u8>();
-                        screen_raw[y0 * width + x0 + 1] = 0;
-                        screen_raw[y0 * width + x0 + 2] = 0;
-                        screen_raw[y0 * width + x0 + 3] = 0xFF;
-                    }
-                }                
             });
+
+            let window = imgui::Window::new(im_str!("CPU"));
+            window
+                .size([400.0, 600.0], Condition::FirstUseEver)
+                .build(&ui, || {
+                    ui.text(format!("PC: {}", self._emulator.pc));
+                });            
+
+        if !self._emulator.is_halting() {
+            self._emulator.execute_instruction()
+        }
+
+        self._emulator.update_timer(ui.io().delta_time);
+
+        for key in 0..16 {
+            self._emulator.keypad.set(key, ui.is_key_down(key as u32));
+        }
+
+        let width = SCREEN_SIZE.0 as usize;
+        let height = SCREEN_SIZE.1 as usize;
+        for x in 0..width {
+            for y in 0..height {
+                let x0 = x * 4;
+                let y0 = y * 4;
+                screen_raw[y0 * width + x0] = if self._emulator.screen.get_pixel(x, y) { 0xFF} else {0};
+                screen_raw[y0 * width + x0 + 1] = 0;
+                screen_raw[y0 * width + x0 + 2] = 0;
+                screen_raw[y0 * width + x0 + 3] = 0xFF;
+            }
+        }
     }
 }
 
-
-fn main()   {
+fn main() {
     let app_desc = app::AppDesc {
         screen_width: SCREEN_SIZE.0,
-        screen_height: SCREEN_SIZE.1
+        screen_height: SCREEN_SIZE.1,
     };
 
     let mut app = Rc::new(MyApp::new());
-    app::run(&app_desc, move |ui: &imgui::Ui, screen_raw: &mut Vec<u8>| {
-       Rc::get_mut(& mut app).unwrap().draw_ui(ui, screen_raw);
-    })
+    app::run(
+        &app_desc,
+        move |ui: &imgui::Ui, screen_raw: &mut Vec<u8>| {
+            Rc::get_mut(&mut app).unwrap().draw_ui(ui, screen_raw);
+        },
+    )
 }
